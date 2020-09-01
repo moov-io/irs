7f79378441b2059e4011feb6872539820a74d929
Adam Shannon <adamkshannon@gmail.com>
2020-09-01 11:35:08 -0500

    database: require json blob, remove metadata

--- migrations/001_documents.up.sql
+++ migrations/001_documents.up.sql
@@ -2,7 +2,7 @@ create table documents(
   document_id varchar(40) primary key not null,
 
   pdf  blob not null,
-  json blob,
+  json blob not null,
 
   created_at timestamp not null,
   deleted_at timestamp
--- migrations/002_document_metadata.up.sql
+++
@@ -1,6 +0,0 @@
-create table document_metadata(
-  document_id varchar(40) primary key not null,
-
-  key   varchar(40) not null,
-  value varchar(512) not null
-);
